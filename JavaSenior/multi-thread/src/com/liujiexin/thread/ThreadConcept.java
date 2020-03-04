f5f40ed674ed4f483bdd0168d06af36b68c0a2bf
ljx <1506095514@qq.com>
2020-03-04 12:00:49 +0800

    新增java高级项目

---
+++ JavaSenior/multi-thread/src/com/liujiexin/thread/ThreadConcept.java
@@ -0,0 +1,42 @@
+package com.liujiexin.thread;
+
+/**
+ * @Description: 多线程的创建
+ *  方式一：继承于Thread
+ *      1.创建一个继承Thread类的子类
+ *      2.重写Thread的run()
+ *      3.创建Thread 类的子类对象
+ *      4.通过此对象调用 start()
+ * @Param: $params$
+ * @return: $returns$
+ * @Author: 张鹏
+ * @Date: $date$
+
+ */
+public class ThreadConcept {
+   Aa aa =  new Aa();
+}
+
+
+
+class Aa extends Thread{
+    @Override
+    public  void run(){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaSenior/src/com/liujiexin/java/Main.java
@@ -0,0 +1,26 @@
+package com.liujiexin.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+
+
+    public  void Ass(){
+        System.out.println("ass"+111);
+    }
+
+
+
+
+
+    /**
+     *
+     *
+     *
+     *
+     */
+
+}
