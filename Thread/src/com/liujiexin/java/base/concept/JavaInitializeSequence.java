12780687cf1db80494bd94f39b127dd8cf2c141f
ljx <1506095514@qq.com>
2020-03-05 15:26:36 +0800

    调整项目结构

---
+++ Thread/src/com/liujiexin/java/base/concept/JavaInitializeSequence.java
@@ -0,0 +1,24 @@
+package com.liujiexin.java.base.concept;
+
+import com.sun.org.glassfish.gmbal.Description;
+
+/**
+ * @program: JavaSenior
+ * @description: java初始化顺序
+ * @author: liu jie xin
+ * @create: 2020-03-05 14:57
+ */
+public class JavaInitializeSequence {
+    
+    
+    /**
+    * @Author Liu Jiexin
+    * @Description
+    * @Date  2020/3/5 15:22
+    * @Param [a]
+    * @return int
+    **/
+    public int As(Integer a){
+        return a + 10;
+    }
+}
---
+++ Thread/src/com/liujiexin/java/base/concept/JavaInitializeSequence1.java
@@ -0,0 +1,11 @@
+package com.liujiexin.java.base.concept;
+
+/**
+ * @Program: JavaSenior
+ * @Description: java 类的初始化顺序
+ * @Author: Liu Jiexin
+ * @Date: 2020-03-05 15:25
+ * @Version 1.0
+ */
+public class JavaInitializeSequence1 {
+}
